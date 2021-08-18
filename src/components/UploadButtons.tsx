1c9498f15e1510f72b11de5dfc11bfc433b31aee
Shiwei Ge <80286832+ShiweiGe1999@users.noreply.github.com>
2021-08-18 14:10:29 -0400

    Form

--- src/components/UploadButtons.tsx
+++ src/components/UploadButtons.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import React from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
+import { useFormikContext } from "formik";
+import { createRef } from "react";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     input: {
-      display: 'none',
+      display: "none",
     },
-  }),
+    noPadding: {
+      padding: "0",
+    },
+  })
 );
+export interface UploadButtonsProps {
+  label: string;
+  onChange: () => void;
+  name: string;
+}
 
-export default function UploadButtons() {
+export default function UploadButtons({
+  onChange,
+  ...props
+}: UploadButtonsProps) {
   const classes = useStyles();
 
   return (
@@ -20,18 +33,18 @@ export default function UploadButtons() {
       <input
         accept="image/*"
         className={classes.input}
-        id="contained-button-file"
-        multiple
+        id="icon-button-file"
         type="file"
+        onChange={onChange}
+        multiple
       />
-      <label htmlFor="contained-button-file">
-        <Button variant="contained" color="primary" component="span">
-          Upload
-        </Button>
-      </label>
-      <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
       <label htmlFor="icon-button-file">
-        <IconButton color="secondary" aria-label="upload picture" component="span">
+        <IconButton
+          color="secondary"
+          aria-label="upload picture"
+          component="span"
+          className={classes.noPadding}
+        >
           <PhotoCamera />
         </IconButton>
       </label>
--- src/pages/addcar.tsx
+++ src/pages/addcar.tsx
@@ -1,14 +1,48 @@
-import { Button, Container, Grid, Paper } from "@material-ui/core";
+import {
+  Avatar,
+  Button,
+  Container,
+  Grid,
+  Paper, Typography
+} from "@material-ui/core";
 import FormControl from "@material-ui/core/FormControl";
+import IconButton from "@material-ui/core/IconButton";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
+import ImageIcon from "@material-ui/icons/Image";
 import { Field, Form, Formik } from "formik";
 import { TextField } from "formik-material-ui";
-import React from "react";
+import React, { useState } from "react";
+import { number, object, string } from "yup";
 import UploadButtons from "../components/UploadButtons";
 
+const useStyles = makeStyles({
+  Avatar: {
+    display: "inline-block",
+  },
+  icon: {
+    textAlign: "right",
+  },
+  borderError: {
+    color: "#ff1744",
+  },
+});
+
 const AddCar = () => {
+  const classes = useStyles();
+  const [files, setFiles] = useState<File[]>();
+  const removeHandler = (index: number) => {
+    setFiles((prev) => {
+      return prev?.filter((e, i) => i != index);
+    });
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFiles(Array.from(event.target.files as ArrayLike<File>));
+  };
   return (
     <Grid container direction="row" justifyContent="center">
       <Grid item xs={12} sm={6} md={3}>
@@ -23,13 +57,49 @@ const AddCar = () => {
                 kilometers: "",
                 details: "",
                 price: "",
-                photoUrl: "",
+                files: [],
               }}
-              onSubmit={(values) => {console.log(values)}}
+              validationSchema={object({
+                make: string()
+                  .max(15, "Must less than 15 characters")
+                  .required("Required"),
+                model: string()
+                  .max(15, "Must less than 15 characters")
+                  .required("Required"),
+                year: number()
+                  .min(1976, "Must be 1976 or later")
+                  .max(
+                    new Date().getFullYear() + 1,
+                    `Newest Model must less than ${
+                      new Date().getFullYear() + 1
+                    }`
+                  )
+                  .required("Required"),
+
+                fuelType: string().matches(/gas|diexel|electric/),
+                kilometers: number().required("Required"),
+                details: string().min(5).required("Required"),
+                price: number().required("Required"),
+              })}
+              onSubmit={(values, actions) => {
+                const formData = new FormData();
+                formData.append("make", values.make);
+                formData.append("model", values.model);
+                formData.append("year", values.year);
+                formData.append("fuelType", values.fuelType);
+                formData.append("kilometers", values.kilometers);
+                formData.append("details", values.details);
+                formData.append("price", values.price);
+                for (let file of values.files) {
+                  formData.append("theFiles", file);
+                }
+                console.log(JSON.stringify(formData))
+                actions.resetForm();
+              }}
             >
-              {({ isSubmitting, values }) => {
+              {({ isSubmitting, values, errors, setFieldValue }) => {
                 return (
-                  <Form autoComplete="off">
+                  <Form>
                     <Grid container item spacing={3}>
                       <Grid item xs={6}>
                         <Field
@@ -60,12 +130,22 @@ const AddCar = () => {
                       </Grid>
                       <Grid item xs={6}>
                         <FormControl fullWidth variant="outlined">
-                          <InputLabel id="fuelType">Fuel Type</InputLabel>
+                          <InputLabel
+                            id="fuelType"
+                            className={
+                              errors.fuelType ? classes.borderError : undefined
+                            }
+                          >
+                            Fuel Type
+                          </InputLabel>
                           <Field
                             as={Select}
                             labelId="Fuel-Type"
                             label="FuelType"
                             name="fuelType"
+                            className={
+                              errors.fuelType ? classes.borderError : null
+                            }
                           >
                             <MenuItem value="all" disabled>
                               <em>Select Fuel Type</em>
@@ -85,7 +165,7 @@ const AddCar = () => {
                       <Grid item xs={6}>
                         <Field
                           component={TextField}
-                          name="Mileage"
+                          name="kilometers"
                           type="number"
                           label="Mileage"
                           fullWidth
@@ -113,14 +193,76 @@ const AddCar = () => {
                         <Grid item>
                           <Field
                             component={UploadButtons}
-                            name="file"
                             label="Simple File Upload"
+                            onChange={(
+                              event: React.ChangeEvent<HTMLInputElement>
+                            ) => {
+                              handleChange(event);
+                              setFieldValue(
+                                "files",
+                                Array.from(
+                                  event.target.files as
+                                    | ArrayLike<File>
+                                    | Iterable<File>
+                                )
+                              );
+                            }}
                             fullWidth
                           />
                         </Grid>
                       </Grid>
+                      <Grid item container spacing={3} xs={12}>
+                        {values.files.map(
+                          (
+                            file: { name: string; [keys: string]: any },
+                            index
+                          ) => {
+                            return (
+                              <Grid
+                                item
+                                container
+                                justifyContent="space-between"
+                                xs={12}
+                                md={6}
+                                key={index}
+                                alignItems="center"
+                              >
+                                <Grid item xs={4}>
+                                  <Avatar>
+                                    <ImageIcon />
+                                  </Avatar>
+                                </Grid>
+                                <Grid item xs={4}>
+                                  <Typography variant="body1">
+                                    {file?.name}
+                                  </Typography>
+                                </Grid>
+
+                                <Grid item xs={4} className={classes.icon}>
+                                  <IconButton
+                                    aria-label="delete"
+                                    type="button"
+                                    onClick={() => {
+                                      const copy = values.files;
+                                      copy.splice(index, 1);
+                                      setFieldValue("files", copy);
+                                    }}
+                                  >
+                                    <DeleteIcon />
+                                  </IconButton>
+                                </Grid>
+                              </Grid>
+                            );
+                          }
+                        )}
+                      </Grid>
                       <Grid item xs={12}>
-                        <Button variant="contained" color="primary" type="submit" fullWidth>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          type="submit"
+                          fullWidth
+                        >
                           Submit
                         </Button>
                       </Grid>
